256eed5a4cb6f19f2b93de69db7a13df855df381
Sandeep Saini <sandeepsaini157219@gmail.com>
2026-04-19 00:33:40 +0530

    html-pdf-node-2

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.post("/generate-brochure", async (req, res) => {
     const options = {
       format: "A4",
       printBackground: true,
-      timeout: 120000, // increase to 2 minutes
+      timeout: 240000, // increase to 2 minutes
       args: [
         "--no-sandbox",
         "--disable-setuid-sandbox",
